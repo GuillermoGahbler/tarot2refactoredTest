9c4a2d0118d1d4619cb8f6e8dbcf032741b4602b
Guillermo Gahbler <mexamericano@gmail.com>
2018-05-31 17:00:49 -0700

    changed cookie timer to 10 mins

--- routes.js
+++ routes.js
@@ -27,7 +27,7 @@ router
     .then(data=>{
       const results = {user: data[0], readings: data[1]}
       res
-        .cookie("name",results.user.username,{maxAge:1000* 60 * 1})
+        .cookie("name",results.user.username,{maxAge:1000* 60 * 10})
         .render("index",{
           ...results,
           logged: req.signedCookies.isLoggedIN,
